7cb553feabe985a4881864e6b6fb9ee3a07e18b4
Atllas1981 <samuelsanchez.rueda@gmail.com>
2026-01-24 11:59:17 -0500

    Agregar dist para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://atllas1981.github.io",
+  base: "/enfermerosBucaramanga/",
   vite: {
     plugins: [tailwind()],
   },
-  site: "https://atllas1981.github.io/enfermerosBucaramanga", // 👈 URL completa
-  base: "/enfermerosBucaramanga/", // 👈 nombre del repo con /
-  outDir: "./dist",
 });
