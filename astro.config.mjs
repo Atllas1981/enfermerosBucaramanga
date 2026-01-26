25d5255ced49823a82a5cf1e3952efdfad18b82b
Atllas1981 <samuelsanchez.rueda@gmail.com>
2026-01-26 08:26:51 -0500

    Deploy Astro usando carpeta docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://atllas1981.github.io",
   base: "/enfermerosBucaramanga/",
+  outDir: "docs",
   vite: {
     plugins: [tailwind()],
   },
